851dc761d543210dbc3747bf67be2b04c242fa41
Rudy Zulkarnain <rzulkarn@yahoo.com>
2020-12-19 09:17:40 -0800

    update nxt_tap to re-inject packet into tun ifce

--- go.mod
+++ go.mod
@@ -1,6 +1,11 @@
 module github.com/rzulkarn/nxt_webtun
 
+go 1.14
+
 require (
+	github.com/google/gopacket v1.1.19 // indirect
 	github.com/songgao/packets v0.0.0-20160404182456-549a10cd4091
 	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
+	golang.org/x/net v0.0.0-20201216054612-986b41b23924 // indirect
+	golang.org/x/sys v0.0.0-20201218084310-7d0127a74742 // indirect
 )
--- nxt_tap.go
+++ nxt_tap.go
@@ -1,36 +1,97 @@
 package main
 
 import (
-	"github.com/songgao/packets/ethernet"
-	"github.com/songgao/water"
+	"encoding/hex"
+	"fmt"
 	"log"
+	"net"
+	"os/exec"
+	"strconv"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/songgao/water"
+	"golang.org/x/net/ipv4"
 )
 
+func ifaceSetup(localCIDR string) *water.Interface {
+	iface, err := water.New(water.Config{DeviceType: water.TUN})
+	if nil != err {
+		log.Println("Unable to allocate TUN interface:", err)
+		panic(err)
+	}
+
+	log.Println("Interface name allocated:", iface.Name())
+
+	if err := exec.Command("ifconfig", iface.Name(), "inet", "10.1.0.1", localCIDR, "mtu", strconv.FormatInt(MTU, 10), "up").Run(); err != nil {
+		log.Fatalln("Unable to setup interface:", err)
+	}
+	return iface
+}
+
 func main() {
-	config := water.Config{
-		DeviceType: water.TAP,
-	}
+	var (
+		snaplen int32 = 65535
+	)
 
-	config.Name = "sample_dev"
+	ifce := ifaceSetup("10.1.0.2")
 
-	ifce, err := water.New(config) // New a tap device
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var frame ethernet.Frame
+	packet := make([]byte, snaplen)
 	for {
-		frame.Resize(1500)
-
-		n, err := ifce.Read([]byte(frame))
+		n, err := ifce.Read(packet)
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Printf("Packet Received %d: % x\n", n, packet[:n])
 
-		frame = frame[:n]
+		// Parse the packet header -
+		header, _ := ipv4.ParseHeader(packet[:n])
+		fmt.Printf("Header struct: %+v (%+v)\n", header, err)
 
-		log.Printf("Dst: %s\n", frame.Destination())
-		log.Printf("Ethertype: %x\n", frame.Ethertype())
-		log.Printf("Payload: %x\n", frame.Payload())
+		packet := gopacket.NewPacket(packet, layers.LayerTypeIPv4, gopacket.Default)
+		if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+			ip := ipLayer.(*layers.IPv4)
+			dst := ip.DstIP.String()
+			src := ip.SrcIP.String()
+			fmt.Printf("Dst: %s, Src: %s\n", dst, src)
+
+			ip.DstIP = net.ParseIP("127.0.0.1")
+
+			options := gopacket.SerializeOptions{
+				ComputeChecksums: true,
+				FixLengths:       true,
+			}
+			newBuffer := gopacket.NewSerializeBuffer()
+			if err = gopacket.SerializeLayers(newBuffer, options,
+				ip,
+			); err != nil {
+				log.Printf("[-] Serialize error: %s\n", err.Error())
+				return
+			}
+
+			outgoingPacket := newBuffer.Bytes()
+			fmt.Println("Hex dump of go packet serialization output:")
+			fmt.Println(hex.Dump(outgoingPacket))
+
+			// write it back into utun interface
+			ifce.Write(outgoingPacket)
+		} else {
+			log.Printf("[-] Not an IPv4 packet\n")
+		}
 	}
 }
+
+//  if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+// 	tcp := tcpLayer.(*layers.TCP)
+// 	dst = fmt.Sprintf("%s:%d", dst, tcp.DstPort)
+// 	src = fmt.Sprintf("%s:%d", src, tcp.SrcPort)
+// 	fmt.Printf("From %s to %s\n\n", src, dst)
+// 	ip.DstIP = net.ParseIP("8.8.8.8")
+// 	newBuffer := gopacket.NewSerializeBuffer()
+// 	gopacket.SerializeLayers(newBuffer, options,
+// 		ip,
+// 		tcp,
+// 	)
+// var frame ethernet.Frame
+// for {
+// 	frame.Resize(1500)
